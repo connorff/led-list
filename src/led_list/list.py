fcac5a63e39d757fb8b61d3cc66aa070bad87b64
Connor Fogarty <connorflynnfogarty@gmail.com>
2023-04-05 11:20:01 -0500

    Cache LED values to speed up light changes

--- src/led_list/list.py
+++ src/led_list/list.py
@@ -1,3 +1,4 @@
+from typing import List, Tuple
 from blinkstick import blinkstick
 from collections import UserList
 import atexit
@@ -22,6 +23,9 @@ class LEDList(UserList):
     # connected blinkstick device
     bstick = blinkstick.find_first()
 
+    # cached value of LED values
+    __cache = []
+
     def __init__(self, initlist):
         super().__init__(initlist)
         
@@ -29,10 +33,37 @@ class LEDList(UserList):
         atexit.register(self.cleanup)
 
         # light up
-        self.__light__()
+        self.__light()
 
-    def __light__(self):
-        for index, led in enumerate(self.data):
+    # find updated LED values between incoming list and cached list
+    @staticmethod
+    def __light_diff(new: List, cached: List) -> List[Tuple]:
+        new = LEDList.__leds_to_tuple(new)
+        updates = []
+
+        # find overlap of new and cached lights
+        overlap = min(len(new), len(cached))
+        for index, values in enumerate(zip(new[0:overlap], cached[0:overlap])):
+            if values[0] != values[1]:
+                updates.append((index, values[0]))
+
+        if len(new) > len(cached):
+            # find lights to turn on
+            additional = new[overlap:]
+        else:
+            # find lights to turn off
+            additional = [(0, 0, 0)] * (len(cached) - overlap)
+
+        updates.extend(enumerate(additional, overlap))
+
+        return updates
+
+    # convert string color values to tuples
+    @staticmethod
+    def __leds_to_tuple(leds: List) -> List[Tuple]:
+        rgbs = []
+
+        for led in leds:
             # convert string colors to tuple
             if isinstance(led, str):
                 if led in PRESET_COLORS:
@@ -48,26 +79,33 @@ class LEDList(UserList):
             if len(led) == 0:
                 led = (0, 0, 0)
 
+            rgbs.append(led)
+
+        return rgbs
+
+    def __light(self):
+        updates = self.__light_diff(self.data, self.__cache)
+
+        for index, led in updates:
             self.bstick.set_color(channel=0, index=index, red=led[0], green=led[1], blue=led[2])
 
-        for led in range(self.NUM_LEDS - len(self.data)):
-            self.bstick.set_color(channel=0, index=len(self.data) + led, name="off")
+        self.__cache = self.__leds_to_tuple(self.data)
 
     def remove(self, item) -> None:
         super().remove(item)
-        self.__light__()
+        self.__light()
 
         return
 
     def append(self, item) -> None:
         super().append(item)
-        self.__light__()
+        self.__light()
 
         return
     
     def extend(self, other):
         super().extend(other)
-        self.__light__()
+        self.__light()
 
         return
 
@@ -77,6 +115,6 @@ class LEDList(UserList):
 
     def __setitem__(self, index, value):
         self.data[index] = value
-        self.__light__()
+        self.__light()
 
         return
